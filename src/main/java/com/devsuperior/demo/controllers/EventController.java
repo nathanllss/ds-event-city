1d998fc3669aadc31937a85ddce5ea415327ae3c
Nathan <nathan.llucasl@gmail.com>
2025-01-20 10:49:08 -0300

    passing all event tests

--- src/main/java/com/devsuperior/demo/controllers/EventController.java
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -1,9 +1,26 @@
 package com.devsuperior.demo.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.services.EventService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
 public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventDTO> updateEvent(@PathVariable Long id, @RequestBody EventDTO dto) {
+        try {
+            EventDTO result = eventService.updateEvent(id, dto);
+            return ResponseEntity.ok(result);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -1,6 +1,11 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.CityRepository;
 import com.devsuperior.demo.repositories.EventRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +14,22 @@ public class EventService {
 
     @Autowired
     private EventRepository eventRepository;
+    @Autowired
+    private CityRepository cityRepository;
+
+    public EventDTO updateEvent(Long id, EventDTO event) {
+        if (!eventRepository.existsById(id)) throw new EntityNotFoundException("Event not found");
+        Event entity = eventRepository.getReferenceById(id);
+        updateProperties(event, entity);
+        entity = eventRepository.save(entity);
+        return new EventDTO(entity);
+    }
+
+    private void updateProperties(EventDTO source, Event target) {
+        target.setName(source.getName());
+        target.setDate(source.getDate());
+        target.setUrl(source.getUrl());
+        target.setCity(cityRepository.findById(source.getCityId()).get());
+    }
+
 }
